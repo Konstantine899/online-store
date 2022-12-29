e33e7062fdfc2a7e348ccc495dade09d9cfb32cf
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-12-29 19:21:10 +0300

    feat: middleware fileSizeLimiter.ts

--- backend/src/modules/Device/routes/deviceRouter.ts
+++ backend/src/modules/Device/routes/deviceRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import DeviceController from "../controllers/deviceController";
 import filePayloadExist from "shared/middleware/filePayloadExist";
+import fileSizeLimiter from "shared/middleware/fileSizeLimiter";
 
 const deviceRouter = Router();
 
-deviceRouter.post("/create", filePayloadExist, DeviceController.create);
+deviceRouter.post("/create", filePayloadExist, fileSizeLimiter, DeviceController.create);
 deviceRouter.get("/all", DeviceController.getAll);
 deviceRouter.get("/:id", DeviceController.getOne);
 deviceRouter.delete("/delete", DeviceController.removeDeviceById);
---
+++ backend/src/shared/middleware/fileSizeLimiter.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Response, Request } from "express";
+import ApiError from "shared/api/ApiError/ApiError";
+
+const MB: number = 5;
+const FILE_SIZE_LIMITER: number = MB * 1024 * 1024;
+
+const fileSizeLimiter = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const files: any = request.files;
+  const fileOverLimit: string[] = [];
+
+  // Определяю какие файлы превышают лимит
+  Object.keys(files).forEach((key) => {
+    if (files[key].size > FILE_SIZE_LIMITER) {
+      fileOverLimit.push(files[key].name);
+    }
+  });
+
+  if (fileOverLimit.length) {
+    const message = `Загружаемый файл ${fileOverLimit} превышен лимит размера файла ${MB} MB.`;
+    return next(ApiError.badRequest(message));
+  }
+  next();
+};
+
+export default fileSizeLimiter;

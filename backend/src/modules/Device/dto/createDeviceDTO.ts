8987e508a29218838b3222945ab1d1bb49662d6e
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-12-30 16:34:13 +0300

    refactor: createDeviceDTO

--- backend/src/modules/Device/dto/createDeviceDTO.ts
+++ backend/src/modules/Device/dto/createDeviceDTO.ts
@@ -15,25 +15,29 @@ export const createDeviceDTO = async (
       request.body;
     const images: any = request.files;
 
+    // Перемещаю картинку в хранилище
     Object.keys(images).forEach((key) => {
       const filepath = path.join(FILES_PATH, images[key].name);
       images[key].mv(filepath, (error: Error) => {
         if (error) return next(ApiError.internal(error.message));
       });
-      createDevice({
-        deviceTypeId: Number(deviceTypeId),
-        deviceBrandId: Number(deviceBrandId),
-        price,
-        name,
-        info,
-        img: images[key].name,
-      });
     });
 
-    return response.json({
-      status: "success",
-      message: `${name} создан успешно`,
+    // Получаю значение картинки из массива
+    const img: string = Object.keys(images)
+      .map((key) => images[key].name)
+      .toString();
+
+    const result = await createDevice({
+      deviceTypeId,
+      deviceBrandId,
+      price,
+      name,
+      info,
+      img,
     });
+
+    return response.json(result);
   } catch (error) {
     console.log(error);
   }
